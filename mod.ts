fc5d9807fe81e00609650a9925de3a66f240e236
urielch <uriel.chemouni@gmail.com>
2024-07-18 09:55:15 +0200

    update doc

--- mod.ts
+++ mod.ts
@@ -10,39 +10,56 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/lu)
  * 
- * ## import_map.json
+ * ## install packahe
  * 
- * For a transparent usage:
- * 
- * ```json
- * {
- *   "imports": {
- *     "@preact-icons/common": "jsr:@preact-icons/common@^1.0.10",
- *     "preact": "npm:preact@10.22.1",
- *     "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *     "preact/hooks": "npm:preact@10.22.1/hooks",
- *     "react-icons/lu": "jsr:@preact-icons/lu@^1.0.11/mod.ts",
- *     "react-icons/lu/": "jsr:@preact-icons/lu@^1.0.11/ico/",
- *   }
- * }
+ * ```bash
+ * deno add @preact-icons/lu
+ * dnpx jsr add @preact-icons/lu
+ * pnpm dlx jsr add @preact-icons/lu
+ * bunx jsr add @preact-icons/lu
  * ```
  * 
- * ## Import an icon without import_map by and afer loading all icons from the lib lu
+ * You may need to update your preact mapping to:
+ *  - "preact": "npm:preact@10.22.1",
+ *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  - "preact/hooks": "npm:preact@10.22.1/hooks",
  * 
- * ```ts
- * import { LuAccessibility } from "jsr:preact-icons/lu@1.0.11/mod.ts"
+ * 
+ * # Lucide icons icons for preact
+ * 
+ * [![JSR](https://jsr.io/badges/@preact-icons/lu)](https://jsr.io/@preact-icons/lu)
+ * 
+ * **License** [ISC License](https://lucide.dev/license)
+ * 
+ * **Project** [https://lucide.dev/](https://lucide.dev/)
+ * 
+ * [See available icons here](https://react-icons.deno.dev/lu)
+ * 
+ * ## install packahe
+ * 
+ * ```bash
+ * deno add @preact-icons/lu
+ * dnpx jsr add @preact-icons/lu
+ * pnpm dlx jsr add @preact-icons/lu
+ * bunx jsr add @preact-icons/lu
  * ```
  * 
- * ## import_map import an icon from all icons
+ * You may need to update your preact mapping to:
+ *  - "preact": "npm:preact@10.22.1",
+ *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  - "preact/hooks": "npm:preact@10.22.1/hooks",
+ * 
+ * 
+ * ## import an icon from all icons
  * 
  * ```ts
- * import { LuAccessibility } from "react-icons/lu"
+ * import { LuAccessibility } from "@preact-icons/lu"
  * ```
  * 
  * ## import a single icon, downloading just one icon
  * 
  * ```ts
- * import { LuAccessibility } from "react-icons/lu/LuAccessibility.ts"
+ * import { LuAccessibility } from "react-icons/lu/LuAccessibility"
  * ```
  * 
  * or using default export
